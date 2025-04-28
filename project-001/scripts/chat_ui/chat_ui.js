1f3a9041a2a50f7f1808392b3af1adaf0c3ad566
bxnxrx <131808453+bxnxrx@users.noreply.github.com>
2025-04-29 01:02:42 +0530

    chat ui stage 3 done , chat clear function done ,overall 70 % done

--- project-001/scripts/chat_ui/chat_ui.js
+++ project-001/scripts/chat_ui/chat_ui.js
@@ -5,6 +5,15 @@ const modeDesc = document.getElementById('mode-desc');
 const modeAvatar = document.getElementById('mode-avatar');
 const avatarImg = document.getElementById('avatar-img');
 const botMessage = document.getElementById('bot-message');
+const newChatBtn = document.getElementById('new-chat-btn');
+const newChatButton = document.getElementById('new-chat-btn');
+const chatMessages = document.getElementById('chat-messages');
+const toggleIcon = document.getElementById('toggle-icon');
+newChatButton.addEventListener('click', () => {
+    chatMessages.innerHTML = '';
+});
+
+
 
 toggle.addEventListener('change', () => {
     if (toggle.checked) {
@@ -14,8 +23,11 @@ toggle.addEventListener('change', () => {
         modeDesc.textContent = 'kiyanna machan (sinhala)';
         modeAvatar.src = '../../assests/akka_chat_ui_upper.png'; // Change this to Akka avatar
         avatarImg.src = '../../assests/akka_giantavatar_chat_ui.png'; // Sidebar avatar
-        botMessage.textContent = 'Nangi, tell me what do you wanna know ?';
+        botMessage.textContent = 'Nangi, කියන්න what do you wanna know ?';
         botMessage.style.backgroundColor = '#f5826e';
+        newChatBtn.style.backgroundColor = '#f5826e';
+        toggleIcon.src = '../../assests/akka_chat_ui_upper.png';
+
     } else {
         // Switch to Ayya mode
         chatHeader.style.backgroundColor = '#6488a7';
@@ -25,5 +37,7 @@ toggle.addEventListener('change', () => {
         avatarImg.src = '../../assests/ayya_giantavatar_chat_ui.png'; // Sidebar avatar
         botMessage.textContent = 'Machan, what\'s on your mind today?';
         botMessage.style.backgroundColor = '#6488a7';
+        newChatBtn.style.backgroundColor = '#6488a7';
+        toggleIcon.src = '.../../assests/ayya_chat_ui_upper.png';
     }
 });
